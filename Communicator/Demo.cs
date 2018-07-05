56ee9a92741dcb07386179678f96dfb66a886b46
Iain Ballard <iain.ballard@gocompare.com>
2018-07-05 12:07:02 +0100

    Cross layer writing, at last

--- Communicator/Demo.cs
+++ Communicator/Demo.cs
@@ -1,10 +1,85 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace Communicator
 {
+
+    [StructLayout(LayoutKind.Sequential)]
+    public class EXTENSION_CONTROL_BLOCK {
+        public Int32     cbSize;                 // size of this struct.
+        public Int32     dwVersion;              // version info of this spec
+        public IntPtr    ConnID;                 // Context number not to be modified!
+        public Int32     dwHttpStatusCode;       // HTTP Status code
+
+        //[MarshalAs(UnmanagedType.LPStr)]
+        public IntPtr lpszLogData; // is this output?
+        //CHAR      lpszLogData[HSE_LOG_BUFFER_LEN];// null terminated log info specific to this Extension DLL
+        
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string     lpszMethod;             // REQUEST_METHOD
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string     lpszQueryString;        // QUERY_STRING
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string     lpszPathInfo;           // PATH_INFO
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string     lpszPathTranslated;     // PATH_TRANSLATED
+
+        public Int32     cbTotalBytes;           // Total bytes indicated from client
+        public Int32     cbAvailable;            // Available number of bytes
+        public IntPtr  /*byte[]*/    lpbData;                // pointer to cbAvailable bytes
+        
+        //[MarshalAs(UnmanagedType.LPStr)]
+        public IntPtr/*string*/     lpszContentType;        // Content type of client data
+
+        public IntPtr A;
+        [MarshalAs(UnmanagedType.FunctionPtr)]
+        public WriteClientDelegate WriteClient;
+        //public IntPtr B;
+        public IntPtr C;
+        public IntPtr D;
+
+        /*
+        [MarshalAs(UnmanagedType.FunctionPtr)]
+        public GetServerVariableDelegate GetServerVariable;
+
+        [MarshalAs(UnmanagedType.FunctionPtr)]
+        public WriteClientDelegate WriteClient;
+
+        [MarshalAs(UnmanagedType.FunctionPtr)]
+        public ReadClientDelegate ReadClient;
+
+        [MarshalAs(UnmanagedType.FunctionPtr)]
+        public ServerSupportFunctionDelegate ServerSupportFunction;*/
+    }
+
+    [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Unicode)]
+    public delegate bool GetServerVariableDelegate(IntPtr hConn,
+                                                    [MarshalAs(UnmanagedType.LPStr)]string lpszVariableName,
+                                                    [MarshalAs(UnmanagedType.LPArray)]byte[] lpvBuffer,
+                                                    ref Int32 lpdwSize);
+    
+    [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Unicode)]
+    public delegate bool WriteClientDelegate(IntPtr ConnID,
+                                            [MarshalAs(UnmanagedType.LPArray)]byte[] Buffer,
+                                            ref Int32 lpdwBytes,
+                                            Int32 dwReserved);
+    
+    [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Unicode)]
+    public delegate bool ReadClientDelegate(IntPtr ConnID,
+                                            [MarshalAs(UnmanagedType.LPArray)]byte[] lpvBuffer,
+                                            ref Int32 lpdwSize);
+    
+    [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Unicode)]
+    public delegate bool ServerSupportFunctionDelegate(IntPtr hConn,
+                                                        Int32 dwHSERequest,
+                                                        [MarshalAs(UnmanagedType.LPArray)]byte[] lpvBuffer,
+                                                        ref Int32 lpdwSize,
+                                                        ref Int32 lpdwDataType);
+
     /// <summary>
     /// Shows the use of C# to drive a C++ shim
     /// </summary>
@@ -12,8 +87,8 @@ namespace Communicator
     {
         // Any delegates should match exactly on the C++ side
         public delegate void VoidDelegate();
-
         public delegate void StringStringDelegate([MarshalAs(UnmanagedType.LPWStr)]string input, [MarshalAs(UnmanagedType.BStr)]out string output);
+        public delegate void HandleHttpRequestDelegate(IntPtr conn, WriteClientDelegate ecb);
 
         /// <summary>Send data back to the host</summary>
         /// <remarks>
@@ -28,6 +103,8 @@ namespace Communicator
         static readonly VoidDelegate ShutdownPtr;
         static GCHandle GcTestDelegateHandle;
         static readonly StringStringDelegate TestPtr;
+        static GCHandle GcHandleRequestDelegateHandle;
+        static readonly HandleHttpRequestDelegate HandlePtr;
 
         /// <summary>
         /// Build our function tables...
@@ -37,7 +114,10 @@ namespace Communicator
             GcShutdownDelegateHandle = GCHandle.Alloc(ShutdownPtr); // prevent garbage collection
 
             TestPtr = TestCallback;
-            GcTestDelegateHandle = GCHandle.Alloc(TestPtr); // prevent garbage collection
+            GcTestDelegateHandle = GCHandle.Alloc(TestPtr);
+
+            HandlePtr = HandleHttpRequestCallback;
+            GcHandleRequestDelegateHandle = GCHandle.Alloc(HandlePtr);
         }
 
         /// <summary>
@@ -50,6 +130,9 @@ namespace Communicator
             switch (requestType) {
                 case "Shutdown":
                     return WriteFunctionPointer(ShutdownPtr);
+
+                case "Handle":
+                    return WriteFunctionPointer(HandlePtr);
 
                 case "Test":
                     return WriteFunctionPointer(TestPtr);
@@ -76,12 +159,41 @@ namespace Communicator
         {
             GcShutdownDelegateHandle.Free();
             GcTestDelegateHandle.Free();
+            GcHandleRequestDelegateHandle.Free();
         }
         
+        static string lastMsg = "No call made";
+        
+        public static void HandleHttpRequestCallback(IntPtr conn, WriteClientDelegate ecb)
+        {
+            var msg = Encoding.UTF8.GetBytes("Hello directly from .Net!\0");
+            int len = msg.Length;
+            ecb(conn, msg, ref len, 0);
+            
+            //var msg = Encoding.UTF8.GetBytes("Hello directly from .Net!");
+            //int leng = msg.Length;
+            //ecb.WriteClient(ecb.ConnID, msg, ref leng, 0);
+            /*try
+            {
+                lastMsg = "\r\nActual pointer: " + ecb.ToString("X");
+
+                var ptr = new IntPtr(ecb);
+                lastMsg += "\r\nDerefd pointer: " + ptr.ToString("X");
+                // var x = Marshal.PtrToStructure<EXTENSION_CONTROL_BLOCK>(ecb);
+                //lastMsg = x.lpszQueryString;
+            }
+            catch (Exception ex)
+            {
+                lastMsg = ex.ToString();
+            }*/
+        }
 
         public static void TestCallback(string input, out string output)
         {
-            output = string.Join("", input.Reverse()); // just a test
+            //Debugger.Launch(); // this doesn't quite work...
+            //output = string.Join("", input.Reverse()); // just a test
+
+            output = lastMsg;
         }
     }
 }
